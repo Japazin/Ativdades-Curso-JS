761091d8e901f955ad0a1a69e94d94465ac673d1
Japazin <mateusfma2bit@gmail.com>
2023-07-23 08:35:39 -0300

    aula2

--- ativ2/ativ2.js
+++ ativ2/ativ2.js
@@ -1,4 +1,4 @@
-const  nome = "Mateus";
-const horas = 18;
-
-console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript às ${horas} da noite`);
+const  nome = "Marcos";
+const dat = (new Date);
+const horas = dat.toLocaleTimeString()
+console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript às ${ horas} da noite`);
