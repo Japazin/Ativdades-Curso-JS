5c9435d28d379926b737b0a6bc8a7fefc5a22896
Japazin <129597459+Japazin@users.noreply.github.com>
2023-07-25 14:53:32 -0300

    arrays

---
+++ ativ5/ativ5.js
@@ -0,0 +1,11 @@
+const tabela = ["Mateus", "teste"]; 
+
+//adiciona ao inicio do array
+tabela.push ("joao");
+//adiciona ao inicio do array
+tabela.unshift("numero0");
+// remove o final pop
+tabela.pop("apagou joao");
+//remove do inicio shift
+tabela.shift("remove numero0");
+console.log(tabela);
