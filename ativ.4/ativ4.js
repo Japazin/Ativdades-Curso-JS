2d86d32017c4a9ed9217c795e87b24e802b60576
Japazin <129597459+Japazin@users.noreply.github.com>
2023-07-25 14:28:28 -0300

    atividade de innerHTML

---
+++ ativ.4/ativ4.js
@@ -0,0 +1,9 @@
+const tex =document.getElementById('texto');
+const num1 = document.getElementById('num');
+let numero1 =Number(prompt("ditige um número:"));
+num1.innerHTML= numero1; 
+// a utilização do += adiciona dados sem apagar os anteriores
+  tex.innerHTML += numero1+2;
+  tex.innerHTML += "teste";
+ 
+
