b5ec778e539ae83f67ebfbe7308e8f167e5c9340
Japazin <129597459+Japazin@users.noreply.github.com>
2023-07-27 14:21:00 -0300

    Update ativ6.js

--- ativ6/ativ6.js
+++ ativ6/ativ6.js
@@ -1,1 +1,20 @@
-console.log("essa aula 06 e um teste de merger de uma branch");
+const objt = {
+    nome:'Mateus',
+    idade:24,
+    area:'Desenvolvimento_full_stack',
+    fala(){
+
+        console.log("digite o primeiro!!");
+    }
+};
+
+// function criaumobjeto (nome, idade){
+//  return {nome, idade};
+// };
+// const tste = criaumobjeto('mateus',24);
+
+
+// if (objt.nome=='Mateus'){
+// return console.log(`${objt.nome} lindo!`)};
+
+objt.fala();
