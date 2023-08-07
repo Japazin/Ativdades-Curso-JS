95b411d1c1410090aefe3a446f3dee0c338156af
Japazin <129597459+Japazin@users.noreply.github.com>
2023-08-07 16:01:15 -0300

    enter na cauculadora

--- calculadora/main.js
+++ calculadora/main.js
@@ -1,41 +1,32 @@
 function criaCalculadora() {
     return {
         display: document.querySelector('.display'),
-  
+
         inicia() {
-            alert("olá mateus");
             this.cliquesBotoes();
+            this.clickEnter();
         },
-        clearDisplay(){
-            this.display.value ='';
-        },
-        apagUm(){
-         this.display.value = this.display.value.slice(0, -1);
-        },
-        resul(){
-            this.display.value = eval(this.display.value)
+        clearDisplay() { this.display.value = '' },
+        apagUm() { this.display.value = this.display.value.slice(0, -1); },
+        resul() { this.display.value = eval(this.display.value) },
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+            this.display.focus();
         },
         cliquesBotoes() {
             document.addEventListener('click', (e) => {
                 const el = e.target;
-                if (el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);
-                }
-                if(el.classList.contains('btn-clear')){
-                    this.clearDisplay();
-                }
-                if (el.classList.contains('btn-del')){
-                    this.apagUm();
-                }
-                if( el.classList.contains('btn-eq')){
-                    this.resul();
-                }
-
+                if (el.classList.contains('btn-num')) this.btnParaDisplay(el.innerText);
+                if (el.classList.contains('btn-clear')) this.clearDisplay();
+                if (el.classList.contains('btn-del')) this.apagUm();
+                if (el.classList.contains('btn-eq')) this.resul();
             });
         },
-        btnParaDisplay(valor) {
-            this.display.value += valor;
-        },
+        clickEnter() {
+            document.addEventListener('keypress', e => {
+                if (e.code === 'Enter') this.resul();
+            })
+        }
     };
 };
 
