e0328778fa56c7ee97e1649d7595c5d416639368
Japazin <129597459+Japazin@users.noreply.github.com>
2023-08-07 17:02:52 -0300

    17:00

--- Callback/black.js
+++ Callback/black.js
@@ -1,21 +1,18 @@
-function f1 (callback){
-    console.log("olá1");
-    if (callback) callback();
-    };
-function f2 (callback){
-console.log("olá2");
-if (callback) callback();
-};
-function f3 (func){
-console.log("olá3");
-if(func)func();
-};
+function f1(callback) {
+  console.log("olá1");
+  if (callback) callback();
+}
+function f2(callback) {
+  console.log("olá2");
+  if (callback) callback();
+}
+function f3(func) {
+  console.log("olá3");
+  if (func) func();
+}
 
-
-f1( function(){
-    f3(function(){
-        f2();
-    });
+f1(function () {
+  f3(function () {
+    f2();
+  });
 });
-
-
--- Html/js.js
+++ Html/js.js
@@ -1,17 +1,17 @@
-const elementos =[
-    {tag:'p',text:'teste de objeto elemento p'},
-    {tag:'div',text:'teste de objeto elemento div'},
-    {tag:'section',text:'teste de objeto elemento section'},
+const elementos = [
+  { tag: "p", text: "teste de objeto elemento p" },
+  { tag: "div", text: "teste de objeto elemento div" },
+  { tag: "section", text: "teste de objeto elemento section" },
 ];
 
-const container = document.querySelector('.container');
-const div = document.createElement('div');
+const container = document.querySelector(".container");
+const div = document.createElement("div");
 
-for(let i =0; i<elementos.length;i++) {
-let { tag, text } =elementos[i];
-let tagcriada = document.createElement(tag);
-tagcriada.innerHTML=text;
-div.appendChild(tagcriada);
-};
+for (let i = 0; i < elementos.length; i++) {
+  let { tag, text } = elementos[i];
+  let tagcriada = document.createElement(tag);
+  tagcriada.innerHTML = text;
+  div.appendChild(tagcriada);
+}
 
 container.appendChild(div);
