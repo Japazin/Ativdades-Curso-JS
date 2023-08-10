7938c8bd47b02050f1d10e68ccb0a975e31d6c35
Japazin <129597459+Japazin@users.noreply.github.com>
2023-08-10 14:55:20 -0300

    Imc atualizado condição NaN

--- Calculo de IMC/ativ3.js
+++ Calculo de IMC/ativ3.js
@@ -1,22 +1,22 @@
 function calculaIMC() {
-    function imctrue() {
-        const prompt = require("prompt-sync")();
-        nome1 = prompt("Digite seu nome:");
-        const alt = prompt("Digite sua altura em metros usando ponto:");
-        const peso = prompt("Digite o seu peso:");
-        imc = peso / (alt * alt);
-        imc = parseInt(imc);
-    }
+  const prompt = require("prompt-sync")();
+  let nome1 = prompt("Digite seu nome:");
 
-    imctrue();
-    let imc;
-    let nome1;
-    if (imc > !0) {
-        console.log(`O seu IMC ${nome1} é de:${imc}`);
-    }
-    while (imc == 0) {
-        console.log("Digite as informações conforme pedidido!!");
-        imctrue();
-    }
+  function imctrue() {
+    const alt = prompt("Digite sua altura em metros usando ponto:");
+    const peso = prompt("Digite o seu peso:");
+    let imc = peso / (alt * alt);
+    return (imc = imc);
+  }
+  let imc = imctrue();
+
+  if (!isNaN(imc)) {
+    console.log(`O seu IMC ${nome1} é de:${imc.toFixed(2)}`);
+    console.log("Obrigado por usar o codigo!")
+  } else {
+    console.log('DIGITE AS INFORMAÇÕES CORRETAS!!')
+    calculaIMC();
+  }
 }
+
 calculaIMC();
