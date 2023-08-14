700190aa8020e76aa388ef4888f95a8bbb039f74
Japazin <129597459+Japazin@users.noreply.github.com>
2023-08-14 11:15:19 -0300

    get set

--- Objet.define.property's/property.js
+++ Objet.define.property's/property.js
@@ -1,11 +1,20 @@
 function ProdutoObjt(nome, preco, estoque) {
-  (this.nome = nome), (this.preco = preco);
+  (this.nome = nome),(this.preco = preco)
   Object.defineProperty(this, "estoque", {
     enumerable: true, // mostra a chave?
     value: estoque, // valor da chave :
     writable: true, // pode alterar?
     configurable: false, // reconfigurável?
   });
+
+  Object.defineProperties(this, {
+    nome:{
+      enumerable: true, // mostra a chave?
+      value: estoque, // valor da chave :
+      writable: true, // pode alterar?
+      configurable: false, // reconfigurável?  
+    }
+  })
 }
 
 const produto1 = new ProdutoObjt("camiseta", 30, 1);
