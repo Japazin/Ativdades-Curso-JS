b5c8f0b921e33b9cfc43f607cb5421f070d7f2e9
Japazin <129597459+Japazin@users.noreply.github.com>
2023-08-21 15:17:39 -0300

    validação de CPF

--- 04 Validar CPF/CPF.js
+++ 04 Validar CPF/CPF.js
@@ -1,18 +1,43 @@
-
-let cpfp = "12345678909";
-//primeira validação mutiplicando os 9 primeiros digitos do CPF com 10 até 2 soma resultado e divide por 11
-
-
-function novedgsoma (cpf){
-    let cpf1= Array.from(cpf);
-    cpf1 = cpf1.splice(0,9);
-    cpf1= cpf1.reduce(function (acul,indice,valor) {
-return acul = acul+valor ;
-    },0);
-
-    console.log(cpf1);
+function CPFválido(cpfp) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function () {
+            cpfp = cpfp.replace(/\D+/g, "");
+            const cpfarray = Array.from(cpfp);
+            const digito1 = this.novedigtosresult(cpfarray);
+            const digito2 = this.dezdigtosresult(cpfarray);
+            const digito1verdadeiro = cpfarray[9];
+            const digito2verdadeiro = cpfarray[10];
+            //validação do CPF
+            return digito1 != digito1verdadeiro || digito2 != digito2verdadeiro
+                ? "CPF inválido!!"
+                : "CPF Válido!!";
+        }
+    })
 }
-novedgsoma(cpfp);
-
-// const result = validaCPF(cpfp);
-// console.log(result);
+CPFválido.prototype.novedigtosresult = function (cpf1) {
+    let cpf = [...cpf1];
+    cpf = cpf.splice(0, 9);
+    contadoor = cpf.length + 2;
+    cpf = cpf.reduce(function (acul, valor) {
+        contadoor = contadoor - 1;
+        return acul + valor * contadoor; //mutiplicação e soma dos 9 digitos
+    }, 0);
+    cpf = cpf % 11;
+    cpf < 2 ? (cpf = 0) : (cpf = 11 - cpf); //verificando se é menor que 2 e retornando 0 se verdadeiro
+    return cpf;
+}
+CPFválido.prototype.dezdigtosresult = function (cpf1) {
+    let cpf = [...cpf1];
+    cpf = cpf.splice(0, 10);
+    contadoor = cpf.length + 2;
+    cpf = cpf.reduce(function (acul, valor) {
+        contadoor = contadoor - 1;
+        return acul + valor * contadoor; //mutiplicação e soma dos 10 digitos
+    }, 0);
+    cpf = cpf % 11;
+    cpf < 2 ? (cpf = 0) : (cpf = 11 - cpf); //verificando se é menor que 2 e retornando 0 se verdadeiro
+    return cpf;
+}
+let result = new CPFválido("705.484.450-52");
+console.log(result);
