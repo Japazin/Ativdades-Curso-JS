107c6cc38446d2604447ba1c544980011f7d7f58
Japazin <129597459+Japazin@users.noreply.github.com>
2023-09-06 16:06:28 -0300

    erro de repeat

--- Validando Formulário/validação.js
+++ Validando Formulário/validação.js
@@ -14,9 +14,40 @@ class ValidaFormula {
   }
   camposValidos() {
     let valid = true;
-    for (let campo of this.formulario.querySelectorAll (".validar")) {
-      console.log(campo);
+
+    for (let erroText of this.formulario.querySelectorAll('.erro-text')) {
+      erroText.remove();
     }
+
+    for (let campo of this.formulario.querySelectorAll(".validar")) {
+      const label = campo.previousElementSibling.innerText;
+
+      if (!campo.value) {
+        this.criaerro(campo, `campo "${label}" não pode estar vazio.`);
+        valid = false;
+      }
+
+      if (campo.classList.contains('CPF')) {
+        if (!this.validacpf(campo)) valid = false;
+      }
+
+    }
+  }
+  validacpf(campo) {
+    const cpf = new ValidaCPF(campo.value);
+
+    if (!cpf.valida()) {
+      this.criaerro(campo, 'CPF inválido.');
+      return false;
+    }
+    return true;
+  }
+
+  criaerro(campo, msg) {
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    div.classList.add('erro-text');
+    campo.insertAdjacentElement('afterend', div);
   }
 }
 const valida = new ValidaFormula();
