03b1d764e00c698c37587915503a293add31a32c
Japazin <129597459+Japazin@users.noreply.github.com>
2023-11-18 13:03:15 -0300

    ok

--- Filter-Map-Reduce/Filter.js
+++ Filter-Map-Reduce/Filter.js
@@ -9,30 +9,30 @@ const terminaemA= pessoas.filter(obj=> obj.nome.toLocaleLowerCase().endsWith("a"
 const nome5indices=pessoas.filter(obj=> obj.nome.length>5);
 console.log(nome5indices);
 console.log(pessoas);
-//ou
+ou
 
-// const arrai =[1,2,3,4,54,56,23,22,676,674,22,2];
-// function callBackFilter(valor,indice,array){
-//     if (valor>100) {
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
-// const maiorq100=arrai.filter(callBackFilter);
-// console.log(maiorq100);
+const arrai =[1,2,3,4,54,56,23,22,676,674,22,2];
+function callBackFilter(valor,indice,array){
+    if (valor>100) {
+        return true;
+    }else{
+        return false;
+    }
+}
+const maiorq100=arrai.filter(callBackFilter);
+console.log(maiorq100);
 
-// OU
+OU
 
-// //  const arrai =[1,2,3,4,54,56,23,22,676,674,22,2];
-// //  const maiorq100=arrai.filter( (valor,indice,array)=>valor>10);
-// //  console.log(maiorq100);
+ const arrai =[1,2,3,4,54,56,23,22,676,674,22,2];
+ const maiorq100=arrai.filter( (valor,indice,array)=>valor>10);
+ console.log(maiorq100);
 
-//OU
+OU
 
-// // const arrai =[1,2,3,4,54,56,23,22,676,674,22,2];
-// //  const maiorq100=arrai.filter( (valor,indice,array)=>{
-// //    console.log(indice,valor);
-// //     return valor>10
-// // });
-// //  console.log(maiorq100);
+const arrai =[1,2,3,4,54,56,23,22,676,674,22,2];
+ const maiorq100=arrai.filter( (valor,indice,array)=>{
+   console.log(indice,valor);
+    return valor>10
+});
+ console.log(maiorq100);
