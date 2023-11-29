701862f0ffaa3198f7032f0c4543dc226083380a
Japazin <129597459+Japazin@users.noreply.github.com>
2023-11-29 14:27:08 -0300

    fetch ok

--- Fetch API(GET)/main.js
+++ Fetch API(GET)/main.js
@@ -1,22 +1,22 @@
-document.addEventListener('click', e=> {
-    const el= e.target;
-    const tag= el.tagName.toLowerCase();
-    
-    if (tag == 'a'){
-        e.preventDefault();
-        carregaPagina(el);
-    }
-});
- function carregaPagina(el){
-    const heref = el.getAttribute('href');
-    
-    fetch(href)
-    .then(response => response.text())
-    .then(html=>{
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
 
-    })
+  if (tag == "a") {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+function carregaPagina(el) {
+  const href = el.getAttribute("href");
+
+  fetch(href)
+    .then(response => response.text())
+    .then( html => carregaResultado(html))
+    .catch(e => console.log(e));
 }
-function carregaPagina(response){
-    const result = document.querySelector('.resultado');
-    resultado.innerHTML = response;
-}
+function carregaResultado(response) {
+  const result = document.querySelector(".resultado");
+  result.innerHTML = response;
+} 
+
